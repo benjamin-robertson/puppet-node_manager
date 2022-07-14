bbec22cb463b482cf47badab7ceac69aee034556
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-14 16:00:02 +1000

    Fix typo

--- lib/puppet/type/node_group.rb
+++ lib/puppet/type/node_group.rb
@@ -35,7 +35,7 @@ Puppet::Type.newtype(:node_group) do
   end
   newproperty(:rule, :array_matching => :all) do
     desc 'Match conditions for this group'
-    default_to []
+    defaultto []
     validate do |value|
       fail("Rules must be specified as an array") unless value.is_a?(Array)
     end
