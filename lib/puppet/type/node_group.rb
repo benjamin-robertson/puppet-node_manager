48c383303207c217732d4a35fdcceaee4eec95f3
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-15 11:58:49 +1000

    Merge rules 1st iteration

--- lib/puppet/type/node_group.rb
+++ lib/puppet/type/node_group.rb
@@ -51,7 +51,21 @@ Puppet::Type.newtype(:node_group) do
           puts a
           puts "This is b"
           puts b
-          merged = (a + b.drop(1)).uniq
+          if b[0] == "or" and b[1][0] == "or" or b[1][0] == "and"
+            puts "We are doing both rules and pinned nodes"
+            rules = (b[1] + a[1].drop(1)).uniq
+            pinned = (a[2,a.length] + b[2,b.length]).uniq
+            b[1] = rules
+            merged = (b + pinned).uniq
+          elsif a[0] == "or" and a[1][0] == "or" or a[1][0] == "and"
+            puts "We are doing both rules and pinned nodes"
+            rules = a[1] # no rules to merge on B side
+            pinned = (a[2,a.length] + b[2,b.length]).uniq
+            merged = (a + pinned).uniq
+          else
+            puts "We are only doing rules OR pinned nodes"
+            merged = (a + b.drop(1)).uniq
+          end
           puts "merged is"
           puts merged
           merged
