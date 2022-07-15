7d35b97e86713df3abaa75647c41df15916623d5
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-15 14:30:05 +1000

    remove array matching

--- lib/puppet/type/node_group.rb
+++ lib/puppet/type/node_group.rb
@@ -33,7 +33,8 @@ Puppet::Type.newtype(:node_group) do
       fail("Variables must be supplied as a hash") unless value.is_a?(Hash)
     end
   end
-  newproperty(:rule, :array_matching => :all) do
+  #newproperty(:rule, :array_matching => :all) do
+  newproperty(:rule) do
     desc 'Match conditions for this group'
     defaultto []
     # validate do |value|
