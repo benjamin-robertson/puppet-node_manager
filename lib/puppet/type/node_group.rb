f0226402628a1c91bf05b7012fa2d72de51aa21e
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-15 15:02:00 +1000

    Added validae and munge

--- lib/puppet/type/node_group.rb
+++ lib/puppet/type/node_group.rb
@@ -36,9 +36,12 @@ Puppet::Type.newtype(:node_group) do
   newproperty(:rule, :array_matching => :all) do
     desc 'Match conditions for this group'
     defaultto []
-    # validate do |value|
-    #   fail("Rules must be specified as an array") unless value.is_a?(Array)
-    # end
+    validate do |value|
+      fail("Rules must be specified as an array") unless value.is_a?(Array)
+    end
+    munge do |value|
+      PuppetX::Node_manager::Common.sort_hash(value)
+    end
     def should
       case @resource[:purge_behavior]
       when :rule, :all
