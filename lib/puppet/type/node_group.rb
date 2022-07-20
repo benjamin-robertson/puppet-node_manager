0c2d454e25712b189f1a64db81e51343e8cc93e8
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-20 15:53:34 +1000

    a drop 1

--- lib/puppet/type/node_group.rb
+++ lib/puppet/type/node_group.rb
@@ -78,7 +78,7 @@ Puppet::Type.newtype(:node_group) do
               puts "a only has rules to merge"
               puts "a is #{a}"
               puts "btmp is #{btmp}"
-              rules = (btmp[1] + a[1]).uniq
+              rules = (btmp[1] + a.drop(1)).uniq
               puts "rules is #{rules}"
               btmp[1] = rules
               puts "btmp is now #{btmp}"
